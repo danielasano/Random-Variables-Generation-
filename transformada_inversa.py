e0677a8d42829f8665ad39d3731b878840d62cc2
Daniela Sano Adathi <daniadathi@gmail.com>
2025-12-15 14:35:40 -0300

    Update transformada_inversa.py

--- transformada_inversa.py
+++ transformada_inversa.py
@@ -2,6 +2,7 @@ import numpy as np
 from random import*
 import matplotlib.pyplot as plt
 from scipy.stats import*
+np.random.seed(123)
 
 lambd = 1
 tam = 1000
@@ -12,3 +13,4 @@ plt.hist(x, bins=20, density=True)
 x = np.linspace(0,8,tam)
 pdf_values = expon.pdf(x, scale=1/lambd)
 plt.plot(x, pdf_values, color='red')
+
